244aa5b90e07f210dd5d1fbef97a0dc9ad949cb9
achhibi <21220829+achhibi@users.noreply.github.com>
2024-06-30 00:31:09 +0200

    Optimization of SimpleVectoreStore

--- src/main/java/net/amch/labs/promptengineeringai/rag/AppConfig.java
+++ src/main/java/net/amch/labs/promptengineeringai/rag/AppConfig.java
@@ -1,16 +1,53 @@
 package net.amch.labs.promptengineeringai.rag;
 
+import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.reader.TextReader;
+import org.springframework.ai.transformer.splitter.TextSplitter;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 @Configuration
 class AppConfig {
 
+    @Value("classpath:rag/rag.txt")
+    private Resource resource;
+    @Value("store.json")
+    private String storeName;
+
     @Bean
-    VectorStore vectorStore(EmbeddingModel embeddingModel) {
-        return new SimpleVectorStore(embeddingModel);
+    VectorStore vectorStore(EmbeddingModel embeddingModel) throws IOException {
+        var vectorStore = new SimpleVectorStore(embeddingModel);
+
+
+        String path = Path.of("src", "main", "resources", "store")
+                .toFile().getAbsolutePath() + File.separator + storeName;
+        File storeFile = new File(path);
+
+        if (storeFile.exists()) {
+            vectorStore.load(storeFile);
+        } else {
+            var textReader = new TextReader(resource);
+            textReader.setCharset(Charset.defaultCharset());
+            List<Document> documents = textReader.get();
+            TextSplitter textSplitter = new TokenTextSplitter();
+            List<Document> chunks = textSplitter.split(documents);
+            vectorStore.add(chunks);
+            vectorStore.save(storeFile);
+        }
+
+        return vectorStore;
     }
 }
--- src/main/java/net/amch/labs/promptengineeringai/rag/RagController.java
+++ src/main/java/net/amch/labs/promptengineeringai/rag/RagController.java
@@ -2,8 +2,6 @@ package net.amch.labs.promptengineeringai.rag;
 
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.messages.UserMessage;
-import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.chat.prompt.SystemPromptTemplate;
 import org.springframework.ai.document.Document;
@@ -13,49 +11,42 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/rag")
 public class RagController {
-    private final RagService ragService;
     private final VectorStore vectorStore;
     private final ChatClient chatClient;
 
-    public RagController(RagService ragService, VectorStore vectorStore, ChatClient.Builder chatClientBuilder) {
-        this.ragService = ragService;
+    public RagController(VectorStore vectorStore, ChatClient.Builder chatClientBuilder) {
         this.vectorStore = vectorStore;
         this.chatClient = chatClientBuilder.build();
     }
 
 
     @GetMapping
-    public String find(String name) {
-        ragService.storeEmbeddingsFromTextFile();
+    public String find(String question) {
         var similarDocuments =
-                vectorStore.similaritySearch(SearchRequest.query(name).withTopK(2));
+                vectorStore.similaritySearch(SearchRequest.query(question).withTopK(2));
+
         String information = similarDocuments.stream()
                 .map(Document::getContent)
                 .collect(Collectors.joining(System.lineSeparator()));
-        System.out.println(similarDocuments);
-        SystemPromptTemplate systemPromptTemplate = new SystemPromptTemplate("""
+
+        PromptTemplate promptTemplate = new SystemPromptTemplate("""
                 You are a helpful assistant.                              
-                Use the following information to answer the question in francais:
-                {information}
+                Use only the following information to answer the question :
+                ----------------------
+                IFORMATION: {information}
+                -----------------------
+                QUESTION: {question}
                 """);
-        var systemMessage = systemPromptTemplate.createMessage(
-                Map.of("information", information));
-        PromptTemplate userMessagePromptTemplate = new PromptTemplate("""
-                Tell me about {name}
-                """);
-        Map<String, Object> model = Map.of("name", name);
-        var userMessage = new UserMessage(userMessagePromptTemplate.create(model).getContents());
 
-        var prompt = new Prompt(List.of(systemMessage, userMessage));
+        Map<String, Object> model = Map.of("information", information, "question", question);
 
-        return chatClient.prompt(prompt).call().content();
+        return chatClient.prompt(promptTemplate.create(model)).call().content();
 
     }
 }
